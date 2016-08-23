6b19e5eb709cb9cc220a75538b5b8d90a3fef187
maticb <matic.blagus@hotmail.com>
2016-08-23 15:59:35 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*
+Ideja:
+Naslov: Vizualizacija uspešnosti evolucijskega računanja v programskem jeziku java script
+
+Input:
+algID; algName;[algParams];problemID; problemName;problemDim;[problemParams]
+{id; [parentids]; timestamp; eval; generation; fitness; [x]}*
+
+GUI:
+Speed (10xslow), load, play
+2dGraph [x_a,x_b] ali [fit, x_a]
+Več grafov;
+*/
+
+function showPopup(){
+
+}
+
+function hidePopup(){
+
+}
+
+$(document).ready(function(){
+
+});
