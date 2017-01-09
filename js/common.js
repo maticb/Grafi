410ac6c0d58771594e1f13af03bb8901c1e2c011
maticb <matic.blagus@hotmail.com>
2017-01-09 22:50:41 +0100

    Moved some functions to common.js

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@
 Example:
 popup.showPopup('<b>hello</b>',[
 {
-	text:'Test',
+	content:'Test',
 	action(){
 		this.hidePopup();
 	}
@@ -15,7 +15,7 @@ popup = {};
 popup.renderPopupButton = function(obj, $appendTo){
 	var self = this;
 	var $btn = $('<button />')
-	$btn.html(obj.text);
+	$btn.html(obj.content);
 	$btn.on('click', function(){
 		obj.action.call(self);
 	});
@@ -40,3 +40,44 @@ popup.hidePopup = function(){
 }
 
 
+/*
+* Loops over all items of a given string/array and triggers callback if item matches value
+* @param string/array 	str 		String or array over which to loop
+* @param string/integer	val 		Value to be compared with looped items
+* @param function 		callback 	Callback function, triggered when item is matched with val
+*/
+function indexOfAll(str, val, callback) {
+	// Compare value function, in case given value is array
+	function compareVal(val1) {
+		if('object' === typeof val){ // If val is array
+			for( var i in val) {
+				if(val[i] === val1)
+					return true;
+			}
+			return false;
+		} else {
+			return val === val1;
+		}
+	}
+	var indexes = [], i;
+	var prev = 0; // Last found index
+	var count = 0; // Counter of matched items
+	for(i = 0; i < str.length; i++){
+		if (compareVal(str[i])){
+			var stop = callback.call(str, i, prev, count);
+			// Check if callback returned true, breaks loop
+			if(true === stop)
+				break;
+			prev = i;
+			count++;
+		}
+	}
+}
+
+/*
+* Universal function that parses array from argument string
+* @param string 	input 		String (JSON) of the given argument
+*/
+function parseArray(input){
+	return JSON.parse(input);
+}
--- js/main.js
+++ js/main.js
@@ -23,48 +23,6 @@ var exampleInput  = ''+
 '{6;2;[2,4];00001;1;3;[2,3,1]}';
 
 /*
-* Loops over all items of a given string/array and triggers callback if item matches value
-* @param string/array 	str 		String or array over which to loop
-* @param string/integer	val 		Value to be compared with looped items
-* @param function 		callback 	Callback function, triggered when item is matched with val
-*/
-function indexOfAll(str, val, callback) {
-	// Compare value function, in case given value is array
-	function compareVal(val1) {
-		if('object' === typeof val){ // If val is array
-			for( var i in val) {
-				if(val[i] === val1)
-					return true;
-			}
-			return false;
-		} else {
-			return val === val1;
-		}
-	}
-	var indexes = [], i;
-	var prev = 0; // Last found index
-	var count = 0; // Counter of matched items
-	for(i = 0; i < str.length; i++){
-		if (compareVal(str[i])){
-			var stop = callback.call(str, i, prev, count);
-			// Check if callback returned true, breaks loop
-			if(true === stop)
-				break;
-			prev = i;
-			count++;
-		}
-	}
-}
-
-/*
-* Universal function that parses array from argument string
-* @param string 	input 		String (JSON) of the given argument
-*/
-function parseArray(input){
-	return JSON.parse(input);
-}
-
-/*
 * Parses entire input text (document)
 * @param string	input 	String input
 */
